9e8cc6c830f0652ac508c8e392bb114cacac9c1b
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-01-03 15:34:26 +0800

    Update meta.js

--- components/meta.js
+++ components/meta.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default ({
   name = 'Innovation Week 2021',
-  title = 'Schedule',
+  title = 'Schedule of Events',
   description = 'A schedule of events and the fun that\'s coming up',
   image = 'https://cloud-cotwl6h2q.vercel.app/0screenshot_2021-01-03_at_3.30.40_pm.png',
   url = 'https://schedule.innovweek.co'
@@ -14,9 +14,6 @@ export default ({
     <meta name="og:url" content={url} />
     <meta property="og:type" content="website" />
     <meta property="og:site_name" content={name} />
-    <meta name="description" content={description} />
-    <meta property="og:description" content={description} />
-    <meta name="twitter:description" content={description} />
     <meta property="og:image" content={image} />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:image" content={image} />
